73ae853cf42fc27ffc139a9b5839c37da54cb8ef
fabrizio marras <fmarras79@gmail.com>
2024-08-24 17:59:04 +0200

    updated tailwind config for Figma basic styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,28 @@
-import type { Config } from "tailwindcss";
+import { Config } from "tailwindcss";
 
-export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#7029df", // White
+        secondary: "#9548f5", // Violet
+      },
+      fontFamily: {
+        text: ["Helvetica", "Arial", "sans-serif"], // Titles
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
