dbae9bfe15bc13c449e16405c9323aee74c076ce
fabrizio marras <fmarras79@gmail.com>
2024-08-24 17:16:22 +0200

    created a thank you page to be loaded after the last lesson is completed

---
+++ app/routes/thank-you.tsx
@@ -0,0 +1,9 @@
+export default function ThankYouPage() {
+    return (
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-4">Congratulations!</h1>
+        <p>You have completed all the lessons in the BADIR framework.</p>
+      </div>
+    );
+  }
+  
